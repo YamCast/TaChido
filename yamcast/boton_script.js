7f1f0a897b5a0bf9343a423c7f97fad680315163
YamCast <100479274+YamCast@users.noreply.github.com>
2022-06-11 08:33:27 -0500

    Add files via upload

---
+++ yamcast/boton_script.js
@@ -0,0 +1,36 @@
+tippy('#myBtn', {
+    content: "invita",
+    placement: 'right',
+    animation: 'fade',
+    theme: 'fondo',
+  });
+
+  
+ tippy('#mylogo', {
+    content: "Inicio",
+    placement: 'right',
+    theme: 'fondo',
+  });
+
+  tippy('#server1', {
+    content: "Servidor 1",
+    placement: 'right',
+    animation: 'perspective',
+    theme: 'fondo',
+  });
+
+  tippy('#server2', {
+    content: "Servidor 2",
+    placement: 'right',
+  });
+
+  tippy('#server3', {
+    content: "Servidor 3",
+    placement: 'right',
+  });
+
+  tippy('#server4', {
+    content: "Servidor 4",
+    placement: 'right',
+  });
+  
