3c56af3a96e767b4e5e310b041b0874863da4d77
YamCast <100479274+YamCast@users.noreply.github.com>
2022-06-18 11:51:11 -0500

    Add files via upload

--- yamcast/boton_script.js
+++ yamcast/boton_script.js
@@ -1,36 +1,58 @@
 tippy('#myBtn', {
-    content: "invita",
-    placement: 'right',
-    animation: 'fade',
-    theme: 'fondo',
-  });
+  content: "invita",
+  placement: 'right',
+  animation: 'fade',
+  animation: 'perspective',
+  theme: 'tomatofondo',
+});
 
-  
- tippy('#mylogo', {
-    content: "Inicio",
-    placement: 'right',
-    theme: 'fondo',
-  });
 
-  tippy('#server1', {
-    content: "Servidor 1",
-    placement: 'right',
-    animation: 'perspective',
-    theme: 'fondo',
-  });
+tippy('#mylogo', {
+  content: "Inicio",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'fondo',
+});
 
-  tippy('#server2', {
-    content: "Servidor 2",
-    placement: 'right',
-  });
+tippy('#server1', {
+  content: "Servidor 1",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'fondo',
+});
 
-  tippy('#server3', {
-    content: "Servidor 3",
-    placement: 'right',
-  });
+tippy('#server2', {
+  content: "Servidor 2",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'fondo',
+});
 
-  tippy('#server4', {
-    content: "Servidor 4",
-    placement: 'right',
-  });
-  
+tippy('#server3', {
+  content: "Servidor 3",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'fondo',
+});
+
+tippy('#server4', {
+  content: "Servidor 4",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'fondo',
+});
+
+tippy('#anadirServidor', {
+  content: "Añadir un servidor",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#explora', {
+  content: "Explora servidores públicos",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
