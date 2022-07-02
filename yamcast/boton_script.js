d459fad4c24d9ef4113851f6896a6ac151aa14cc
YamCast <100479274+YamCast@users.noreply.github.com>
2022-07-02 02:00:16 -0500

    Add files via upload

--- yamcast/boton_script.js
+++ yamcast/boton_script.js
@@ -1,45 +1,53 @@
-tippy('#myBtn', {
+tippy('#myBtn', '#mylogo', {
   content: "invita",
   placement: 'right',
   animation: 'fade',
   animation: 'perspective',
-  theme: 'tomatofondo',
+  theme: 'tomato',
+});
+
+
+tippy('#myBtn', {
+  content: "Inicio",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
 });
 
 
 tippy('#mylogo', {
-  content: "Inicio",
+  content: "Invita",
   placement: 'right',
   animation: 'perspective',
-  theme: 'tomato-theme',
+  theme: 'tomato',
 });
 
 tippy('#server1', {
   content: "Servidor 1",
   placement: 'right',
   animation: 'perspective',
-  theme: 'tomato-theme',
+  theme: 'tomato',
 });
 
 tippy('#server2', {
   content: "Servidor 2",
   placement: 'right',
   animation: 'perspective',
-  theme: 'fondo',
+  theme: 'tomato',
 });
 
 tippy('#server3', {
   content: "Servidor 3",
   placement: 'right',
   animation: 'perspective',
-  theme: 'fondo',
+  theme: 'tomato',
 });
 
 tippy('#server4', {
   content: "Servidor 4",
   placement: 'right',
   animation: 'perspective',
-  theme: 'fondo',
+  theme: 'tomato',
 });
 
 tippy('#anadirServidor', {
@@ -52,7 +60,38 @@ tippy('#anadirServidor', {
 tippy('#explora', {
   content: "Explora servidores públicos",
   placement: 'right',
-  animation: 'perspective',
+  animation: 'fade',
   theme: 'tomato',
 });
 
+
+tippy('descarg', {
+  content: "Descarga Aplicaciones",
+  placement: 'right',
+  animation: 'fade',
+  theme: 'tomato',
+});
+
+
+tippy('micro', {
+  content: "Activar sondo",
+  animation: 'fade',
+  theme: 'tomato',
+});
+tippy('auricu', {
+  content: "Ensordecer",
+  animation: 'fade',
+  theme: 'tomato',
+});
+tippy('ajustes', {
+  content: "Ajustes de Usuario",
+  animation: 'fade',
+  theme: 'tomato',
+});
+
+
+tippy('#bot', {
+  content: "Bot Verificado",
+  animation: 'fade',
+  theme: 'tomato',
+});
